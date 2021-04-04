9b3b88e857f22eb61589e9495a0f2280c360550b
EstefanoVilela <60205135+EstefanoVilela@users.noreply.github.com>
2021-04-03 20:40:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,63 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link rel="stylesheet" href="./bootstrap.css">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Reconocimiento de voz</title>
+	<link rel="stylesheet" href="./bootstrap.css">
 </head>
 
 <body>
-    <div class="container pt-5">
-        <button type="button" id="btnStart" class="btn btn-primary">GRABAR</button>
-        <button type="button" id="btnStop" class="btn btn-danger">DETENER</button>
+	<div class="container pt-5">
+		<div class="row justify-content-center">
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-header">
+						<h5 class="title">TEST RECORDING</h5>
+					</div>
+					<div class="card-body">
+						<textarea name="texto" id="texto" rows="10" class="form-control"></textarea>
+					</div>
+					<div class="card-footer">
+						<button type="button" id="btnStart" class="btn btn-primary btn-block">Grabar</button>
+						<button type="button" id="btnStop" class="btn btn-danger btn-block">Detener</button>
+						<button type="button" id="btnPlayText" class="btn btn-success btn-block">Reproducir</button>
+					</div>
+				</div>
+			</div>
+		</div>
     </div>
 
-    <script>
-        var btnStart = document.getElementById("btnStart");
-        var rec = new webkitSpeechRecognition();
-
-        if("webkitSpeechRecognition" in window){
-            console.log("Todo bien!");
-
-            rec.lang = "es-PE";
-            rec.continuous = true;
-            rec.interim = true;
-
-            rec.addEventListener("result", resultadoGrabacion);
-            rec.addEventListener("error", errorGrabacion);
-        }else{
-            console.error("No se puede usar la API");
-        }
-
-        function resultadoGrabacion(event){
-            console.log('\n\n==========\n\nonresult\n----------\n');
-
-            console.log('Transcript' + event.results[event.results.length-1][0].transcript);
-            console.log('Confidence: ' + event.results[event.results.length-1][0].confidence);
-            console.log('results' + event.results);
-
-            console.log('Tú has dicho: '+event.results[event.results.length-1][0].transcript);
-        }
-
-        function errorGrabacion(event){
-            console.error("errorGrabacion");
-            console.error(event.error);
-        }
-
-        btnStart.addEventListener('click', () => {
-            console.log("GRABAR!");
-            rec.start();
-        });
-
-        btnStop.addEventListener('click', () => {
-            console.log("DETENER!");
-            rec.stop();
-        });
-    </script>
+    <script src="./app.js"></script>
 </body>
 
 </html>
